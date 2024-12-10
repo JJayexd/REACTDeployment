652081a93295f4086e1b0b97ccc6988d2abbd2ed
JJayexd <kontakt.emilbj@gmail.com>
2024-12-10 08:58:56 +0100

    sigma

--- src/Components/Livretter/Livretter.jsx
+++ src/Components/Livretter/Livretter.jsx
@@ -1,18 +1,8 @@
-// export const Livretter = () => {
-//     return (
-//         <ul>
-//             {LivretterA.map((livret) => {
-//                 return <li>{livret}</li>
-//             })}
-//         </ul>
-//     )
-// }
-
 export const Livretter = ({ Livretter }) => {
     return (
         <ul>
-            {Livretter.map((livret) => {
-                return <li key={livret}>{livret}</li>
+            {Livretter.map((livret, key) => {
+                return <li key={key}>{livret}</li>
             })}
         </ul>
     )
